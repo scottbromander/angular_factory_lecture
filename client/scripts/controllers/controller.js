921834b8357e9b20f0c9e413c30a05a562a522a1
Scott Bromander <scott@primeacademy.io>
2015-11-13 11:49:29 -0600

    Visual update

--- client/scripts/controllers/controller.js
+++ client/scripts/controllers/controller.js
@@ -1,21 +1,26 @@
 myApp.controller('BaseController', ["$scope", "DataService", function($scope, DataService){
-
+    $scope.people = [];
     $scope.dataService = DataService;
 
     if($scope.dataService.peopleData() === undefined){
         $scope.dataService.retrieveData();
     }
+
+    $scope.people = $scope.dataService.peopleData();
 
     console.log("Base Async: " , $scope.dataService.peopleData());
 }]);
 
 myApp.controller('AnotherController', ["$scope", "DataService", function($scope, DataService){
+    $scope.people = [];
 
     $scope.dataService = DataService;
 
     if($scope.dataService.peopleData() === undefined){
         $scope.dataService.retrieveData();
     }
+
+    $scope.people = $scope.dataService.peopleData();
 
     console.log("Another Async: " , $scope.dataService.peopleData());
 }]);
